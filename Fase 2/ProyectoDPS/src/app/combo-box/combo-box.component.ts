1e2273c0acf07f5beaf917429ab032218c85c52a
ManuelGamez <gamezmanuel.progra17@gmail.com>
2020-09-05 11:36:10 -0600

    Inicio del proyecto

---
+++ Fase 2/ProyectoDPS/src/app/combo-box/combo-box.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {BrowserModule}from '@angular/platform-browser' 
+import {FormsModule} from '@angular/forms'
+
+@Component({
+  selector: 'app-combo-box',
+  templateUrl: './combo-box.component.html',
+  styleUrls: ['./combo-box.component.css']
+})
+export class ComboBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
